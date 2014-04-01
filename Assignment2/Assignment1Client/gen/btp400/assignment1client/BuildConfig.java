f8368f352b4bcf8f95bb0addb2b1d04f698f8bd5
kabeer25 <kabeer.rashid25@gmail.com>
2014-03-31 23:53:09 -0400

    Assignment 2 Updated

---
+++ Assignment2/Assignment1Client/gen/btp400/assignment1client/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package btp400.assignment1client;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Assignment2/Lab9/gen/btp400/lab9/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package btp400.lab9;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
